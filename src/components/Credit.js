d84727b688eb270b179de9ae1787c2bfdc378520
Your name here <mohdwaseem2831@gmail.com>
2024-02-10 11:35:32 +0530

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.linkedin.com/in/mohammed-waseem-20a752207/"
+        > Mohammed Waseem</a
+      >, <a href="https://github.com/mohammedwaseem18/Wheather-app">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
